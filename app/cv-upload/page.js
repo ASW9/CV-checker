80e9708f04d6b41139962d91601e951207e830b7
Owen Lee Scott <Fumwa23@outlook.com>
2024-01-30 22:01:39 +0000

    Deleted unnecessary code from page.js

--- app/cv-upload/page.js
+++ app/cv-upload/page.js
@@ -4,51 +4,8 @@ import { FilePond } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import Image from 'next/image';
 import FileUpload from '@/components/FileUpload';
-import Customisation from '@/components/PersonaCreator';
 
 const ChatPage = () => {
-
-    const [openFeedbackBox, setOpenFeedbackBox] = useState(false)
-    const toggleFeedbackBox = () => {
-        setOpenFeedbackBox(true)
-    }
-    // const form = document.querySelector('form');
-
-    // function handleSubmit(event) {
-    //     event.preventDefault();
-
-    //     uploadFiles();
-    // }
-
-    // function uploadFiles() {
-    //     const url = 'https://httpbin.org/post';
-    //     const method = 'post';
-
-    //     const xhr = new XMLHttpRequest();
-
-    //     const data = new FormData(form);
-
-    //     xhr.open(method, url);
-    //     xhr.send(data);
-    // }
-
-    // const uploadButtonClick = () => {
-    //     //In this function we need to trigger a pop up that allows them to upload a file
-
-    //     //then we will call the convertToHTML function with that file. 
-    // }
-
-    // async function convertToHTML () {
-    //     // const pdf2html = require('pdf2html');
-    //     // const html = await pdf2html.html('sample.pdf');
-    //     // console.log(html);
-    // }
-    
-
-    //heading - done
-    //subtitle - done
-    //better font
-    //cool gradient on background (behind card, i.e full page behind card) -
 
     return (
         <>
@@ -80,7 +37,6 @@ const ChatPage = () => {
                             }}
                             credits={{label:'', url:''}}
                         />
-                        <Customisation toggleOpen={toggleFeedbackBox} open={openFeedbackBox} config={personaConfig} updateConfig={setPersonaConfig}/>
                     </div>
             </div>
 
