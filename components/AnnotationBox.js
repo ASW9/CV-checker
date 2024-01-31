7fc491b059109e617027c371ddff2b0f47238827
Owen Lee Scott <Fumwa23@outlook.com>
2024-01-31 04:45:38 +0000

    Slowly changing look of floating annotation box

--- components/AnnotationBox.js
+++ components/AnnotationBox.js
@@ -2,9 +2,8 @@ import React from "react";
 
 const AnnotationBox = (props) => {
 
-
     return (
-        <div className="flex flex-col rounded-b-xl bg-white">
+        <div className="flex flex-col rounded-xl bg-white border-gray-500 border-solid border-2 items-center h-64 w-64 p-4" >
             {props.text}
         </div>
     );
