5fc953b5e9676d410e8a0227c3a880801af5851e
Owen Lee Scott <Fumwa23@outlook.com>
2024-01-31 16:15:04 +0000

    Created new page for list of CVs

---
+++ app/cv/page.js
@@ -0,0 +1,24 @@
+"use client"
+import { useEffect, useRef, useState } from 'react';
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import Image from 'next/image';
+import FileUpload from '@/components/FileUpload';
+import StatsDial from '@/components/Stats';
+import AnnotationComponent from '@/components/AnnotationsPage';
+import { Feed } from '@mui/icons-material';
+import UpdatedCVComponent from '@/components/UpdatedCV';
+
+const cvPage = () => {
+
+    return (
+        <>
+            <div>
+                wow so empty
+            </div>
+        </>
+  );
+
+};
+
+export default cvPage;
