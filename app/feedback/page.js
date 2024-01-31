6984a6abf1a264914c07a1ed35a890b47cf27686
Owen Lee Scott <Fumwa23@outlook.com>
2024-01-31 03:04:55 +0000

    Updated the titles in the stats component and changed the background colour.

--- app/feedback/page.js
+++ app/feedback/page.js
@@ -10,10 +10,7 @@ const FeedbackPage = () => {
 
     return (
         <>
-        <div className='flex flex-col h-screen w-screen items-center p-16 justify-center bg-gradient-to-br from-gray-900 via-gray-400 to-gray-200'>
             <StatsDial />
-        </div>
-        
         </>
   );
 
--- components/Stats.js
+++ components/Stats.js
@@ -5,18 +5,14 @@ import Image from 'next/image';
 // By default, it's using your primary color for styling
 const StatsDial = ({ children }) => {
   return (
-    <section id="testimonials">
+    <section className="bg-base-200">
       <div className="py-24 px-8 max-w-7xl mx-auto">
         <div className="flex flex-col justify-items-center text-center w-full mb-20">
           <div className="mb-8">
-            <h2 className="sm:text-5xl text-4xl font-extrabold text-base-content">
+            <h2 className="max-w-3xl mx-auto font-extrabold text-4xl md:text-5xl tracking-tight mb-6 md:mb-8">
               Where you could improve
             </h2>
           </div>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-base-content/80 pb-12">
-            Don&apos;t take our word for it. Here&apos;s what they have to say
-            about ShipFast.
-          </p>
           <div className="flex flex-col w-full justify-items-center">
             <Image
               src="/dials.png"
