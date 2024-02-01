c50a9e8d968e120710509945fadcc8e6514e5488
Owen Lee Scott <Fumwa23@outlook.com>
2024-02-01 00:32:21 +0000

    Changed spacing of annotation bubbles

--- components/AnnotationsPage.js
+++ components/AnnotationsPage.js
@@ -25,19 +25,19 @@ const AnnotationComponent = () => {
                   />
               </div>
               <div className="flex flex-col pr-20">
-                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-50 transform hover:translate-x-[-3rem]'}>
+                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-[14rem] transform hover:translate-x-[-3rem]'}>
                     You may want to omit your GCSE grades and focus on your recent qualifications and experiences.
                   </div>
-                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-6 transform hover:translate-x-[-3rem]'}>
+                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-[3rem] transform hover:translate-x-[-3rem]'}>
                     Your CV would be more effective and reader friendly if you use concise bullet points instead of lengthy paragraphs
                   </div>
                   <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-6 transform hover:translate-x-[-3rem]'}>
                     Review your text for typos to ensure a polished and professional impression
                   </div>
-                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-6 transform hover:translate-x-[-3rem]'}>
+                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-[5rem] transform hover:translate-x-[-3rem]'}>
                     Consider moving some of this detail to a cover letter to keep your resume concise and focused.
                   </div>
-                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-6 transform hover:translate-x-[-3rem]'}>
+                  <div className={'hover:shadow-lg rounded-xl bg-gray-100 items-center text-xs w-64 p-4 hover:scale-105 ease-in-out duration-500 mt-[9rem] transform hover:translate-x-[-3rem]'}>
                     Some of this information may not be relevant to the position you are applying for.
                   </div>
               </div>
